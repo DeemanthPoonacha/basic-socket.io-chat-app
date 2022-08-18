5c1c31a7375b535fa2d46225d04f817cbb6fa2aa
DeemanthKP <deemanth.p@tataelxsi.co.in>
2022-08-18 14:40:31 +0530

    initial commit

---
+++ react-client/src/components/Chat.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+function Chat({ socket, username, roomId }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        roomId: roomId,
+        author: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      setMessageList((list) => [...list, messageData]);
+      console.log(messageList);
+      await socket.emit("send_message", messageData);
+      setCurrentMessage("");
+    }
+  };
+  
+    
+  useEffect(() => {
+    const eventListener = (data) => {
+        setMessageList((list) => [...list, data]);
+    };
+    socket.on("receive_message", eventListener);
+
+    return () => socket.off("receive_message", eventListener);
+  }, [socket]);
+    
+//   useEffect(() => {
+//     socket.on("receive_message", (data) => {
+//       setMessageList((list) => [...list, data]);
+//     });
+//   }, []);
+
+//   useEffect(() => {
+//     socket.on("receive_message", (data) => {
+//     //   setMessageList((list) => [...list, data]);
+//         console.log(data);
+//     });
+//   }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat: {roomId}</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+                <div
+                key={messageContent.message+messageContent.time}
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author===username?<>You</>:messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Message..."
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
